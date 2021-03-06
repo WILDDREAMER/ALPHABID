58f89aa9440ddc0bd54c2133c0c4cb33472caa0c
OUSSAMA ZAKKARE <53281129+WILDDREAMER@users.noreply.github.com>
2021-03-06 16:25:05 +0100

    test

---
+++ react-front-end/src/components/home/index.js
@@ -0,0 +1,11 @@
+import Hero from './Hero'
+
+export default function Home(params) {
+    return(
+        <div className='home'>
+            <Hero>
+                
+            </Hero>
+        </div>
+    )
+}
